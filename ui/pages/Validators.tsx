214eb133b4a17dd60ea98a3f5c9351b5435f8429
root <root@vmi2365865.contaboserver.net>
2024-12-28 14:52:08 +0100

    Revert "footer changes"
    This reverts commit 1ec55519fde0763febb5edc5c3f7b8f1c8a1c179.

--- ui/pages/Validators.tsx
+++ ui/pages/Validators.tsx
@@ -1,5 +1,4 @@
 import { Box, Hide, HStack, Show } from '@chakra-ui/react'; // Correct import
-import { callApi } from 'customApi/api';
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
 
@@ -7,7 +6,9 @@ import useIsMobile from 'lib/hooks/useIsMobile';
 import ActionBar from 'ui/shared/ActionBar';
 import DataListDisplay from 'ui/shared/DataListDisplay';
 import PageTitle from 'ui/shared/Page/PageTitle';
+
 import ValidatorsCounters from 'ui/validators/ValidatorsCounters';
+import { callApi } from 'customApi/api';
 
 interface Validator {
   _id: string;
@@ -25,24 +26,26 @@ interface ValidatorResponse {
   totalValidators: number;
   totalPages: number;
   currentPage: number;
-  data: Array<Validator>;
+  data: Validator[];
   message: string;
 }
 
 const Validators = () => {
-
+  
+ 
   // const router = useRouter();
   const [ validatorsData, setValidatorsData ] = useState([]); // Placeholder, fix with types if needed
   const [ totalPages, setTotalPages ] = useState(1);
   const [ currentPage, setCurrentPage ] = useState(1);
   const [ sort, setSort ] = useState<string | undefined>(undefined);
 
+
   useEffect(() => {
-    callApi()?.then((res) => {
-      console.log(res, 5656);
-    }).catch((err) => {
-      console?.log(err, 5656);
-    });
+    callApi()?.then((res)=>{
+      console.log(res,5656);
+    }).catch((err)=>{
+      console?.log(err,5656);
+    })
 
   }, []);
 
@@ -66,44 +69,44 @@ const Validators = () => {
 
   const actionBar = (
     <>
-      <HStack spacing={ 3 } mb={ 6 } display={{ base: 'flex', lg: 'none' }}>
-        { /* {filterMenu}
-        {sortButton} */ }
+      <HStack spacing={3} mb={6} display={{ base: 'flex', lg: 'none' }}>
+        {/* {filterMenu}
+        {sortButton} */}
       </HStack>
-      <ActionBar mt={ -6 }>
-        <HStack spacing={ 3 } display={{ base: 'none', lg: 'flex' }}>
-          { /* {filterMenu} */ }
+      <ActionBar mt={-6}>
+        <HStack spacing={3} display={{ base: 'none', lg: 'flex' }}>
+          {/* {filterMenu} */}
         </HStack>
-        { /* <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} /> */ }
+        {/* <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} /> */}
       </ActionBar>
     </>
   );
 
   const content = validatorsData.length > 0 ? (
     <>
-      <Show below="lg" ssr={ false }>
-        { /* <ValidatorsList data={validatorsData} /> */ }
+      <Show below="lg" ssr={false}>
+        {/* <ValidatorsList data={validatorsData} /> */}
       </Show>
-      <Hide below="lg" ssr={ false }>
-        { /* <ValidatorsTable data={validatorsData} sort={sort} setSorting={handleSortChange} /> */ }
+      <Hide below="lg" ssr={false}>
+        {/* <ValidatorsTable data={validatorsData} sort={sort} setSorting={handleSortChange} /> */}
       </Hide>
     </>
   ) : null;
 
   return (
     <Box>
-      <PageTitle title="Validators" withTextAd/>
+      <PageTitle title="Validators" withTextAd />
       <ValidatorsCounters/>
       <DataListDisplay
-        isError={ !validatorsData.length }
-        items={ validatorsData }
+        isError={!validatorsData.length}
+        items={validatorsData}
         emptyText="There are no verified validators."
         // filterProps={{
         //   emptyFilteredText: "Couldn't find any validator that matches your query.",
         //   // hasActiveFilters: Boolean(statusFilter),
         // }}
-        content={ content }
-        actionBar={ actionBar }
+        content={content}
+        actionBar={actionBar}
       />
     </Box>
   );
